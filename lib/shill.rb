1ee7f35969e2dcfe0fdbd13dda9765b471411c96
Marc Köhlbrugge <subscriptions@marckohlbrugge.com>
2025-05-15 12:50:11 +0100

    Initial commit
    Add gem structure with essential files including .gitignore, gemspec, README, and basic test setup.

---
+++ lib/shill.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "shill/version"
+
+module Shill
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/shill/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Shill
+  VERSION = "0.1.0"
+end
